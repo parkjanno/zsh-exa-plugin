73cb4b128ebeb847d4ef16c41aec02049830ffde
parkjanno <73113278+parkjanno@users.noreply.github.com>
2022-09-07 16:01:20 -0400

    Update zsh-exa.plugin.zsh

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -1,8 +1,3 @@
-if ! (( $+commands[exa] )); then
-  print "zsh-exa-plugin: exa not found on path. Please install exa before using this plugin." >&2
-  return 1
-fi
-
 alias ls='exa --color-scale --icons --group-directories-first --time-style=long-iso'
 alias l='ls -lbF'
 alias la='ls -lbhigUma'
